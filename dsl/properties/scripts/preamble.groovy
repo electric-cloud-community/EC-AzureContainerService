b5e8b87c87b899dd7d2e562b496f9c7abe5dfd25
Vishal Biyani <vishal@infracloud.io>
2017-10-27 12:25:44 +0530

    Added support for DockerCE orchastrator

--- dsl/properties/scripts/preamble.groovy
+++ dsl/properties/scripts/preamble.groovy
@@ -2,7 +2,6 @@ $[/plugins/EC-Kubernetes/project/scripts/Logger]
 $[/plugins/EC-Kubernetes/project/scripts/BaseClient]
 $[/plugins/EC-Kubernetes/project/scripts/EFClient]
 $[/plugins/EC-Kubernetes/project/scripts/KubernetesClient]
+$[/plugins/EC-Docker/project/scripts/DockerClient]
 
 $[/myProject/scripts/AzureClient]
-
-
