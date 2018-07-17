a32002e20f363f04d8981414d2e0ce0a6659146b
selectrics <sserguey@electric-cloud.com>
2018-07-17 10:54:14 +0300

    ECAZCS-65

--- dsl/properties/ec_container_service_plugin/clusterTopology/script.groovy
+++ dsl/properties/ec_container_service_plugin/clusterTopology/script.groovy
@@ -5,14 +5,14 @@ def config = args.configurationParameters
 
 def credentials = args.credential
 assert credentials.size() == 1
-def userName = credentials[0].userName
-def password = credentials[0].password
+//def userName = credentials[0].userName
+//def password = credentials[0].password
 
-def endpoint = config.clusterEndpoint
-def token = password
-def version = config.kubernetesVersion
-assert endpoint
-assert version
+//def endpoint = config.clusterEndpoint
+//def token = password
+//def version = config.kubernetesVersion
+//assert endpoint
+//assert version
 
 def cluster = getCluster(projectName: projectName, environmentName: environmentName, clusterName: clusterName)
 def clusterId = cluster.clusterId.toString()
@@ -21,7 +21,24 @@ import com.electriccloud.errors.EcException
 import com.electriccloud.errors.ErrorCodes
 import com.electriccloud.kubernetes.*
 
-def client = new Client(endpoint, token, version)
+def clusterParameters = getProvisionClusterParameters(
+        clusterName,
+        projectName,
+        environmentName)
+
+String azAccessToken = Client.retrieveAccessToken(pluginConfig)
+String masterFqdn = Client.getMasterFqdn(config.subscriptionId, clusterParameters.resourceGroupName, clusterParameters.clusterName, azAccessToken)
+String clusterEndPoint = "https://${masterFqdn}"
+def privateKey = efClient.getCredentials("${configName}_keypair")
+String accessToken = Client.retrieveOrchestratorAccessToken(pluginConfig,
+        clusterParameters.resourceGroupName,
+        clusterParameters.clusterName,
+        azAccessToken,
+        clusterParameters.adminUsername,
+        masterFqdn,
+        privateKey.password)
+def version = Client.getVersion(clusterEndPoint, accessToken)
+def client = new Client(clusterEndPoint, accessToken, version)
 assert clusterId
 assert clusterName
 def clusterView = new ClusterView(kubeClient: client,
@@ -30,6 +47,7 @@ def clusterView = new ClusterView(kubeClient: client,
         clusterName: clusterName,
         clusterId: clusterId)
 def response
+
 try {
     response = clusterView.getRealtimeClusterTopology()
 } catch (EcException e) {
