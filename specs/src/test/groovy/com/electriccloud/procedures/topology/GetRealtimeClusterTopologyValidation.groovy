af0ba3d7fa9ef87ba5746b8d1c9a3473a40dc3b5
Savva Genchevskiy <savva.genchevskiy@gmail.com>
2018-10-10 12:53:02 +0300

    Update GetRealtimeClusterTopologyValidation.groovy

--- specs/src/test/groovy/com/electriccloud/procedures/topology/GetRealtimeClusterTopologyValidation.groovy
+++ specs/src/test/groovy/com/electriccloud/procedures/topology/GetRealtimeClusterTopologyValidation.groovy
@@ -7,6 +7,7 @@ import org.testng.annotations.BeforeClass
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
+import org.testng.annotations.BeforeMethod
 
 
 
