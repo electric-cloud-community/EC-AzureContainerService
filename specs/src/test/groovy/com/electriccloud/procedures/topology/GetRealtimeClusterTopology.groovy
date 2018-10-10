5f181d3694bbd4fb175f87d85a3a8b450a901caa
Savva Genchevskiy <savva.genchevskiy@gmail.com>
2018-10-10 12:52:44 +0300

    Update GetRealtimeClusterTopology.groovy

--- specs/src/test/groovy/com/electriccloud/procedures/topology/GetRealtimeClusterTopology.groovy
+++ specs/src/test/groovy/com/electriccloud/procedures/topology/GetRealtimeClusterTopology.groovy
@@ -6,6 +6,7 @@ import io.qameta.allure.*
 import org.testng.annotations.AfterClass
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
+import org.testng.annotations.BeforeMethod
 
 import static com.electriccloud.helpers.enums.LogLevels.*
 
